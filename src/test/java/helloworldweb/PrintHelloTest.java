3edc1f4e2f670d7e1eb9236dcea79c32438c14b8
kanchangxin <kanchangxin@139.com>
2015-09-13 00:08:13 +0800

    first commit

---
+++ src/test/java/helloworldweb/PrintHelloTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package helloworldweb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author changxin
+ *
+ */
+public class PrintHelloTest {
+
+	/**
+	 * Test method for {@link helloworldweb.PrintHello#process(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
+	 */
+	@Test
+	public void testProcess() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
